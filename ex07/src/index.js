c91153fa2308213ad3de8686360668b56c462fce
mirza-z <zaimovic330@gmail.com>
2022-04-15 16:23:51 +0200

    Hello World

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,8 +1,18 @@
 // Only change code below this line
 let numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
-let uniqueArray = [...new Set(numberArray.sort())];
-console.log(uniqueArray.sort((function (a, b) { return a - b })));
+
+let uniqueArray = [];
+let mySet = new Set();
+
+for (let i = 0; i < numberArray.length; i++) {
+    mySet.add(numberArray[i]);
+}
+
+for (let num of mySet) {
+    uniqueArray.push(num);
+}
+uniqueArray.sort((a, b) => a - b)
+console.log(uniqueArray);
 
 // Only change code above this line
-
 module.exports = { numberArray, uniqueArray };
--- ex08/src/index.js
+++ ex08/src/index.js
@@ -3,8 +3,8 @@
 let one = new Set([1, 2, 3, 4, 5]);
 let two = new Set([6, 4, 3, 2]);
 
-unionSet = new Set([...one, ...two]);
-intersectionSet = new Set(
+let unionSet = new Set([...one, ...two]);
+let intersectionSet = new Set(
     [...one].filter(x => two.has(x)
     ));
 differenceSet = new Set(
